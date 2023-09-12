04b727d5060a989777500bbb416795cdd70b76ec
Claire Guiot <cguiot@nic.bc.ca>
2023-09-11 18:39:58 -0700

    fix functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,11 @@
-
-
 <?php
-
-/* enqueue scripts and style from parent theme */
- 
-function twentytwentyone_styles() {
-wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-array( 'twenty-twenty-one-style' ), wp_get_theme()->;get('Version') );
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 
+		'child-style', 
+		get_stylesheet_uri(),
+		array( 'twenty-twenty-one-style' ),
+		wp_get_theme()->get( 'Version' ) 
+	);
 }
-add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
 ?>
